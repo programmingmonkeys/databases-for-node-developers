513a8f15763d1c07d952c5c1c4a23e00d26b5c49
codingChewie <matthewtimothy910@yahoo.com>
2019-09-23 13:31:39 -0500

    s03e08 - Define your first model

--- shopsy/server/models/mongoose/User.js
+++ shopsy/server/models/mongoose/User.js
@@ -3,14 +3,19 @@ const bcrypt = require('bcryptjs');
 
 const SALT_WORK_FACTOR = 10;
 
-
 const UserSchema = mongoose.Schema({
   email: {
     // Trim and lowercase
-    type: String, required: true, index: { unique: true }, lowercase: true, trim: true,
+    type: String,
+    required: true,
+    index: { unique: true },
+    lowercase: true,
+    trim: true,
   },
   password: {
-    type: String, required: true, trim: true,
+    type: String,
+    required: true,
+    trim: true,
   },
 }, { timestamps: true });
 
@@ -39,6 +44,5 @@ UserSchema.methods.comparePassword = function comparePassword(candidatePassword,
     return cb(null, isMatch);
   });
 };
-
 
 module.exports = mongoose.model('User', UserSchema);
